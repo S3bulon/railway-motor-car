985e9a59408928efb0639e9455ae6c9018595ec5
S3bulon <d3r_max@web.de>
2022-06-20 22:10:18 +0200

    added mod-support for ignored locomotives for motorcars, and ignored "Super locomotive"

--- src/data.lua
+++ src/data.lua
@@ -1,4 +1,9 @@
 shared = require "shared"
 
+-- ignore some locomotives for dynamic motor cars
+railway_motorcar_ignored = {
+  "ee-super-locomotive", -- EditorExtensions
+}
+
 require "prototypes.motorcar"
 require "prototypes.equipment"
