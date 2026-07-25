774b6f9dc42d11edd337f16a9a0c2271f7f7fecc
S3bulon <d3r_max@web.de>
2026-07-25 15:10:23 +0200

    update to factorio 2.1 update to jetpack 0.5.1 compatibility for cargo ships mod remove support for old traintunnels mod

--- src/scripts/compatibility.lua
+++ src/scripts/compatibility.lua
@@ -6,16 +6,16 @@ function compatibility.can_mount(player)
 
   -- jetpack: cannot mount if jetpack is in use (only actual flying)
   local jetpacks = remote.interfaces["jetpack"] and remote.call("jetpack", "get_jetpacks", {surface_index=player.surface.index})
-  if jetpacks and jetpacks[player.character.unit_number] and jetpacks[player.character.unit_number].status == "flying" then
+  if jetpacks and jetpacks[player.character.unit_number] then
     player.create_local_flying_text({ text = { "flying-text."..shared.name.."-jetpack-in-use" }, position = player.position })
     return false
   end
 
-  -- train tunnels: cannot mount b/c <enter vehicle> is used for changing surfaces
-  if script.active_mods["traintunnels"] and table_size(player.surface.find_entities_filtered({
+  -- cargo ships: cannot mount waterways which are rails internally
+  if script.active_mods["cargo-ships"] and table_size(player.surface.find_entities_filtered({
     position = player.position,
-    radius = 10, -- large radius
-    name = { "traintunnel", "traintunnelup", "traintunneldown" }
+    radius = 4,
+    name = { "straight-waterway", "curved-waterway-a", "curved-waterway-b", "half-diagonal-waterway" }
   })) > 0 then
     return false
   end
